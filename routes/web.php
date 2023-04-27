b2b262b1704065e58cc31acb3b9fb5f2a89ccbfd
masuryasa <suryasa.made@gmail.com>
2023-04-27 11:34:04 +0800

    add redirect on route index

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return redirect('/books');
+});
 
 Route::get('/books', [BookController::class, 'index']);
 
