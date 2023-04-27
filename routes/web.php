1e32118c551b6b6961e94216dd5d323080bd9e8e
masuryasa <suryasa.made@gmail.com>
2023-04-27 15:03:19 +0800

    edit action name show to detail

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/books', [BookController::class, 'index']);
 
-Route::get('/books/detail', [BookController::class, 'show']);
+Route::get('/books/detail', [BookController::class, 'detail']);
